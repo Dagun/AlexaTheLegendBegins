3d6955747119ff1639e886c4b1d7eec04ae26a83
Eduard Stetco <eduard.stetco@htlpinkafeld.at>
2018-07-21 09:19:53 +0200

    Removed alex in the code and added grassFull.png

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "SDL_image.h"
 
 using namespace std;
-Alex
+
 SDL_Texture *LoadTexture(char *filePath, SDL_Renderer *renderTarget) {
   SDL_Texture *texture = NULL;
   SDL_Surface *surface = IMG_Load(filePath);
@@ -108,4 +108,3 @@ int main(int argc, char *argv[])
     SDL_Quit();
     return 0;
 }
-Alex
